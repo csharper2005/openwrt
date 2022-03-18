a7aff56fa49a028e7aa3914280f6a450479dd608
Mikhail Zhilkin <csharper2005@gmail.com>
2022-03-18 18:09:45 +0000

    ramips: add support for Beeline SmartBox TURBO
    Beeline SmartBox TURBO is a wireless WiFi 5 router manufactured by
    Sercomm company.
    
    Device specification
    --------------------
    SoC Type: MediaTek MT7621AT
    RAM: 256 MiB
    Flash: 256 MiB, Micron MT29F2G08ABAGA3W
    Wireless 2.4 GHz (MT7603EN): b/g/n, 2x2
    Wireless 5 GHz (MT7615E): a/n/ac, 4x4
    Ethernet: 5xGbE (WAN, LAN1, LAN2, LAN3, LAN4)
    USB ports: 1xUSB3.0
    Button: 2 buttons (Reset & WPS)
    LEDs: 1 RGB LED
    Power: 12 VDC, 1.5 A
    Connector type: barrel
    Bootloader: U-Boot
    
    Installation
    -----------------
    1.  Login to the router web interface (admin:admin)
    2.  Navigate to Settings -> WAN -> Add static IP interface (e.g.
        10.0.0.1/255.255.255.0)
    3.  Navigate to Settings -> Remote cotrol -> Add SSH, port 22,
        10.0.0.0/255.255.255.0 and interface created before
    4.  Change IP of your client to 10.0.0.2/255.255.255.0 and connect the
        ethernet cable to the WAN port of the router
    5.  Connect to the router using SSH shell (SuperUser:SNxxxxxxxxxx, where
        SNxxxxxxxxxx is the serial number from the backplate label)
    6.  Run in SSH shell:
           sh
    7.  Make a mtd backup (optional, see related section)
    8.  Change bootflag to Sercomm1 and reboot:
           printf 1 | dd bs=1 seek=7 count=1 of=/dev/mtdblock3
           reboot
    9.  Login to the router web interface (admin:admin)
    10. Remove dots from the OpenWrt factory image filename
    11. Update firmware via web using OpenWrt factory image
    
    Revert to stock
    ---------------
    1. Change bootflag to Sercomm1 in OpenWrt CLI and then reboot:
          printf 1 | dd bs=1 seek=7 count=1 of=/dev/mtdblock3
    2. Optional: Update with any stock (Beeline) firmware if you want to
       overwrite OpenWrt in Slot 0 completely.
    
    mtd backup
    ----------
    1. Set up a tftp server (e.g. tftpd64 for windows)
    2. Connect to a router using SSH shell and run the following commands:
          cd /tmp
          for i in 0 1 2 3 4 5 6 7 8 9 10; do nanddump -f mtd$i /dev/mtd$i; \
          tftp -l mtd$i -p 10.0.0.2; md5sum mtd$i >> mtd.md5; rm mtd$i; done
          tftp -l mtd.md5 -p 10.0.0.2
    
    MAC Addresses
    -------------
    +-----+-----------+---------+
    | use | address   | example |
    +-----+-----------+---------+
    | LAN | label     | *:54    |
    | WAN | label + 1 | *:55    |
    | 2g  | label + 4 | *:58    |
    | 5g  | label + 5 | *:59    |
    +-----+-----------+---------+
    The label MAC address was found in Factory 0x21000
    
    Co-developed-by: Maximilian Weinmann <x1@disroot.org>
    Signed-off-by: Maximilian Weinmann <x1@disroot.org>
    Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -56,6 +56,7 @@ platform_do_upgrade() {
 	asus,rt-ax53u|\
 	beeline,smartbox-flash|\
 	beeline,smartbox-giga|\
+	beeline,smartbox-turbo|\
 	dlink,dap-x1860-a1|\
 	dlink,dir-1960-a1|\
 	dlink,dir-2640-a1|\
