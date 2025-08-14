07e3c67972699eb6cf7aea548ac54f9820a6cd0d
Mikhail Zhilkin <csharper2005@gmail.com>
2025-08-14 22:37:08 +0300

    mediatek: CMCC RAX3000M: disable u-boot build
    mediatek: CMCC RAX3000M: disable u-boot build
    
    Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -616,15 +616,6 @@ define Device/cmcc_rax3000m
   IMAGE/sysupgrade.itb := append-kernel | \
 	 fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | \
 	 pad-rootfs | append-metadata
-  ARTIFACTS := \
-	emmc-gpt.bin emmc-preloader.bin emmc-bl31-uboot.fip \
-	nand-preloader.bin nand-bl31-uboot.fip
-  ARTIFACT/emmc-gpt.bin := mt798x-gpt emmc
-  ARTIFACT/emmc-preloader.bin := mt7981-bl2 emmc-ddr4
-  ARTIFACT/emmc-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000m-emmc
-  ARTIFACT/nand-preloader.bin := mt7981-bl2 spim-nand-ddr4
-  ARTIFACT/nand-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000m-nand
-  DEFAULT := n
 endef
 TARGET_DEVICES += cmcc_rax3000m
 
