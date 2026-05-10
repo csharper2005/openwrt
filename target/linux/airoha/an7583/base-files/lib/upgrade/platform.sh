f932f2956f5c3661a116d08f2144ee25713a9c96
Mikhail Zhilkin <csharper2005@gmail.com>
2026-05-10 17:15:55 +0300

    airoha: add Nokia XG-040G-MF support
    Add Nokia XG-040G-MF support.
    
    Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>

---
+++ target/linux/airoha/an7583/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2026 OpenWrt.org
+#
+
+REQUIRE_IMAGE_METADATA=1
+
+platform_check_image() {
+	local board=$(board_name)
+
+	[ "$#" -gt 1 ] && return 1
+
+	case "$board" in
+	nokia,xg-040g-mf)
+		# tar magic `ustar`
+		magic="$(dd if="$1" bs=1 skip=257 count=5 2>/dev/null)"
+
+		[ "$magic" != "ustar" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+
+		return 0
+		;;
+	*)
+		nand_do_platform_check "$board" "$1"
+		return $?
+		;;
+	esac
+
+	return 0
+}
+
+platform_do_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+		*)
+			nand_do_upgrade "$1"
+			;;
+	esac
+}
+
+platform_pre_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	buffalo,wsr-6000ax8)
+		buffalo_initial_setup
+		;;
+	xiaomi,mi-router-ax3000t|\
+	xiaomi,mi-router-wr30u-stock|\
+	xiaomi,redmi-router-ax6000-stock)
+		xiaomi_initial_setup
+		;;
+	esac
+}
--- target/linux/airoha/image/an7583.mk
+++ target/linux/airoha/image/an7583.mk
@@ -16,6 +16,21 @@ define Device/airoha_an7583-evb
 endef
 TARGET_DEVICES += airoha_an7583-evb
 
+define Device/nokia_xg-040g-mf
+  $(call Device/FitImageLzma)
+  DEVICE_VENDOR := Nokia
+  DEVICE_MODEL := XG-040G-MF
+  DEVICE_PACKAGES := airoha-an7583-npu-firmware kmod-phy-airoha-en8811h \
+	kmod-regulator-userspace-consumer kmod-usb-ledtrig-usbport \
+	kmod-usb3 ubootenv-tools
+  DEVICE_DTS := an7583-nokia_xg-040g-mf
+  DEVICE_DTS_CONFIG := config@1
+  KERNEL_LOADADDR := 0x80200000
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  SOC := an7583
+endef
+TARGET_DEVICES += nokia_xg-040g-mf
+
 define Device/airoha_an7583-evb-emmc
   DEVICE_VENDOR := Airoha
   DEVICE_MODEL := AN7583 Evaluation Board (EMMC)
