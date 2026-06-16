ef05213f44ad1ae65641ed61d32f45b500902a11
Mikhail Zhilkin <csharper2005@gmail.com>
2026-06-16 08:12:57 +0300

    airoha: an7583: remove source-only flag
    Remove source-only flag from Airoha an7583 subtarget for upcoming Nokia
    XG-040G-MF support. Also disable builds for airoha_an7583-evb and
    airoha_an7583-evb-emmc evaluation boards.
    
    Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>
    (cherry picked from commit 9e7415c63980016f9ff8dcc7d9808be9af0e6990)

--- target/linux/airoha/an7583/target.mk
+++ target/linux/airoha/an7583/target.mk
@@ -3,7 +3,7 @@ SUBTARGET:=an7583
 BOARDNAME:=AN7583
 CPU_TYPE:=cortex-a53
 KERNELNAME:=Image dtbs
-FEATURES+=pwm source-only
+FEATURES+=pwm
 
 define Target/Description
 	Build firmware images for Airoha an7583 ARM based boards.
