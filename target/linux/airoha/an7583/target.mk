78b929eb86f36667732a8d81659d008cfe1ebdd0
Mikhail Zhilkin <csharper2005@gmail.com>
2026-06-16 08:20:20 +0300

    airoha: an7583: add default packages
    Add airoha-an7583-npu-firmware and uboot-envtools to the default
    packages.
    
    Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>
    (cherry picked from commit 5fa7c9b753df7fc423d9ad754c6ed78d6070d9cd)

--- target/linux/airoha/an7583/target.mk
+++ target/linux/airoha/an7583/target.mk
@@ -5,6 +5,9 @@ CPU_TYPE:=cortex-a53
 KERNELNAME:=Image dtbs
 FEATURES+=pwm
 
+DEFAULT_PACKAGES += \
+    airoha-an7583-npu-firmware uboot-envtools
+
 define Target/Description
 	Build firmware images for Airoha an7583 ARM based boards.
 endef
